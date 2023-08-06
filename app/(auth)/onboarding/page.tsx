80edc8b830dee52a3cfbd8b1d38552a404e6df5a
Colin Hunwald <colin.hunwald@gmail.com>
2023-08-06 19:49:25 +0200

    Add shadcn

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,11 +1,30 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+import { currentUser } from '@clerk/nextjs/server';
 import React from 'react';
 
 type Props = {};
 
 async function Page({}: Props) {
+  const user = await currentUser();
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || '',
+    bio: userInfo?.bio || '',
+    image: userInfo?.image || user?.imageUrl,
+  };
   return (
-    <main>
+    <main className="flex flex-col justify-start max-w-3xl px-10 py-20 mx-auto">
       <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text.base-regular text-light-2">
+        Complete your profile now to use Threads
+      </p>
+      <section className="p-10 mt-9 bg-dark-2">
+        <AccountProfile user={userData} btnTitle="continue" />
+      </section>
     </main>
   );
 }
