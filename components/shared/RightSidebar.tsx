d5de5d445225792ac48a0c6188cc216b884327b4
Colin Hunwald <colin.hunwald@gmail.com>
2023-08-06 15:41:01 +0200

    Navigation

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,5 +1,16 @@
 const RightSidebar = () => {
-  return <h1>RightSidebar</h1>;
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-col justify-start flex-1">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested communities
+        </h3>
+      </div>
+      <div className="flex flex-col justify-start flex-1">
+        <h3 className="text-heading4-medium text-light-1">Suggested users</h3>
+      </div>
+    </section>
+  );
 };
 
 export default RightSidebar;
