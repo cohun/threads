9dd4a4a26ff237de37bec4896fee795accf9b7f4
Colin Hunwald <colin.hunwald@gmail.com>
2023-08-06 14:57:22 +0200

    Initial commit

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+async function Page({}: Props) {
+  return (
+    <main>
+      <h1 className="head-text">Onboarding</h1>
+    </main>
+  );
+}
+
+export default Page;
